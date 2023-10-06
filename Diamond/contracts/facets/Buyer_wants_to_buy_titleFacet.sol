71e3ff891bee51c09e2807ec45dc4c780809c230
Dilum Bandara <dilum.bandara@csiro.au>
2023-10-06 15:05:15 +1100

    Dilum's comments

--- Diamond/contracts/facets/Buyer_wants_to_buy_titleFacet.sol
+++ Diamond/contracts/facets/Buyer_wants_to_buy_titleFacet.sol
@@ -6,6 +6,15 @@ import {LibDiamond} from "../libraries/LibDiamond.sol";
 contract Buyer_wants_to_buy_titleFacet {
     event taskCompleted(string taskName);
 
+    // Facets don't need to know processvariablesfacetaddress as all state must be on diamond proxy
+    // like the "This diagram shows the structure of a diamond:" figure in https://eips.ethereum.org/EIPS/eip-2535
+    address processvariablesfacetaddress = 0x0000000000000000000000000000000000000000;
+
+    // Like proxies shouldn't facets not have a constuctor
+    constructor(address _processvariablesfacetaddress) {
+        processvariablesfacetaddress = _processvariablesfacetaddress;
+    }
+
     function Buyer_wants_to_buy_title() public returns (bool) {
         uint predicates = 0;
         if ((LibDiamond.preconditions() & 0x8 == 0x8)) {
