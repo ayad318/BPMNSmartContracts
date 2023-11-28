c0e9d6d7f087198cd48d1bf63f3bbb8f820cb0e9
Dilum Bandara <dilum.bandara@csiro.au>
2023-11-28 15:17:44 +1100

    Merge Aday's changes

--- Diamond/contracts/facets/Buyer_wants_to_buy_titleFacet.sol
+++ Diamond/contracts/facets/Buyer_wants_to_buy_titleFacet.sol
@@ -6,6 +6,11 @@ import {LibDiamond} from "../libraries/LibDiamond.sol";
 contract Buyer_wants_to_buy_titleFacet {
     event taskCompleted(string taskName);
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> ayad318-master
     // Facets don't need to know processvariablesfacetaddress as all state must be on diamond proxy
     // like the "This diagram shows the structure of a diamond:" figure in https://eips.ethereum.org/EIPS/eip-2535
     address processvariablesfacetaddress = 0x0000000000000000000000000000000000000000;
@@ -15,6 +20,7 @@ contract Buyer_wants_to_buy_titleFacet {
         processvariablesfacetaddress = _processvariablesfacetaddress;
     }
 
+>>>>>>> 1e7fc53 (Dilum's comments)
     function Buyer_wants_to_buy_title() public returns (bool) {
         uint predicates = 0;
         if ((LibDiamond.preconditions() & 0x8 == 0x8)) {
---
+++ Diamond/contracts/facets/ProcessVariablesFacet.sol
@@ -0,0 +1,153 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {LibDiamond} from "../libraries/LibDiamond.sol";
+
+// Shouldn't these be on the diamond proxy like the figure in https://eips.ethereum.org/EIPS/eip-2535?
+contract ProcessVariablesFacet {
+    uint preconditions = 0x1;
+
+    // ---------- PROCESS VARIABLES
+    uint consignmentWeight;
+    bool isLoanAccepted;
+    bool titleCreationSuccess;
+    bool isDirectSale;
+    bytes32 titleId;
+    bool titleTransferSuccess;
+    uint truckWeightWithoutConsignment;
+    address buyer;
+    uint truckWeightWithConsignment;
+    uint grainQuality;
+    // ----------------------------
+
+    // -------- EXTERNAL SMART CONTRACT ADDRESSES TODO: Update the address
+    address addressOfGrainTitle_Registry = 0x72a239b360041a1964A7BA3d36FF735498F4d203;
+
+    // ------------------------------------
+
+    constructor() {
+        consignmentWeight = 0;
+        isLoanAccepted = false;
+        titleCreationSuccess = false;
+        isDirectSale = false;
+        titleId = 0x0;
+        titleTransferSuccess = false;
+        truckWeightWithoutConsignment = 0;
+        buyer = 0x0000000000000000000000000000000000000000;
+        truckWeightWithConsignment = 0;
+        grainQuality = 0;
+    }
+
+    function Calculate_grain_weight(uint preconditionsp) internal returns (uint) {
+        if ((preconditionsp & 0x8000 == 0x8000)) {
+            return (preconditionsp & ~uint(0x8000)) | 0x4;
+        } else {
+            consignmentWeight = truckWeightWithConsignment - truckWeightWithoutConsignment;
+            return preconditionsp;
+        }
+    }
+
+    function step(uint preconditionsp) external {
+        if (preconditionsp & 0xffff == 0) {
+            preconditions = 0;
+            return;
+        }
+        bool done = false;
+        while (!done) {
+            if ((preconditionsp & 0x8000 == 0x8000)) {
+                preconditionsp = Calculate_grain_weight(preconditionsp);
+                continue;
+            }
+            done = true;
+        }
+        preconditions = preconditionsp;
+    }
+
+    function getPreconditions() external view returns (uint) {
+        return preconditions;
+    }
+
+    function setConsignmentWeight(uint _consignmentWeight) external {
+        consignmentWeight = _consignmentWeight;
+    }
+
+    function getConsignmentWeight() external view returns (uint) {
+        return consignmentWeight;
+    }
+
+    function setIsLoanAccepted(bool _isLoanAccepted) external {
+        isLoanAccepted = _isLoanAccepted;
+    }
+
+    function getIsLoanAccepted() external view returns (bool) {
+        return isLoanAccepted;
+    }
+
+    function setTitleCreationSuccess(bool _titleCreationSuccess) external {
+        titleCreationSuccess = _titleCreationSuccess;
+    }
+
+    function getTitleCreationSuccess() external view returns (bool) {
+        return titleCreationSuccess;
+    }
+
+    function setIsDirectSale(bool _isDirectSale) external {
+        isDirectSale = _isDirectSale;
+    }
+
+    function getIsDirectSale() external view returns (bool) {
+        return isDirectSale;
+    }
+
+    function setTitleId(bytes32 _titleId) external {
+        titleId = _titleId;
+    }
+
+    function getTitleId() external view returns (bytes32) {
+        return titleId;
+    }
+
+    function setTitleTransferSuccess(bool _titleTransferSuccess) external {
+        titleTransferSuccess = _titleTransferSuccess;
+    }
+
+    function getTitleTransferSuccess() external view returns (bool) {
+        return titleTransferSuccess;
+    }
+
+    function setTruckWeightWithoutConsignment(uint _truckWeightWithoutConsignment) external {
+        truckWeightWithoutConsignment = _truckWeightWithoutConsignment;
+    }
+
+    function getTruckWeightWithoutConsignment() external view returns (uint) {
+        return truckWeightWithoutConsignment;
+    }
+
+    function setBuyer(address _buyer) external {
+        buyer = _buyer;
+    }
+
+    function getBuyer() external view returns (address) {
+        return buyer;
+    }
+
+    function setTruckWeightWithConsignment(uint _truckWeightWithConsignment) external {
+        truckWeightWithConsignment = _truckWeightWithConsignment;
+    }
+
+    function getTruckWeightWithConsignment() external view returns (uint) {
+        return truckWeightWithConsignment;
+    }
+
+    function setGrainQuality(uint _grainQuality) external {
+        grainQuality = _grainQuality;
+    }
+
+    function getGrainQuality() external view returns (uint) {
+        return grainQuality;
+    }
+
+    function getGrainTitleRegistryAddress() external view returns (address) {
+        return addressOfGrainTitle_Registry;
+    }
+}
